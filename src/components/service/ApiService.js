644d7dc179dbcb34b43eb5ff81dcb4dd4dd7a337
İsa ŞENGÜL <sengulisa49@icloud.com>
2025-03-06 15:19:16 +0300

    added some api method

--- src/components/service/ApiService.js
+++ src/components/service/ApiService.js
@@ -4,6 +4,7 @@ export default class ApiService{
 
     static BASE_URL1 ="http://localhost:8080/finance-mgmt"
     static BASE_URL2 = "http://localhost:8081/finance-mgmt"
+    static BASE_URL3 = "http://localhost:8084/finance-mgmt"
 
     static getHeader (){
         const token = localStorage.getItem("token");
@@ -25,6 +26,20 @@ export default class ApiService{
 
     static async GetAccountInfoByUsername(username){
         const response = await axios.get(`${this.BASE_URL2}/api/account/get-account/${username}`);
+        return response.data;
+    }
+    static async moneyTransfer(transferInfo){
+        const response = await axios.post(`${this.BASE_URL2}/api/account/money-transfer`,transferInfo);
+        return response.data;
+    }
+    static async GetAllUsers(){
+        const response = await axios.get(`${this.BASE_URL3}/api/users`);
+        return response.data;
+    }
+    static async GetUserById(userİd){
+        const response = await axios.get(`${this.BASE_URL3}/api/users`,{
+            params:{id:userİd}
+        });
         return response.data;
     }
 }
